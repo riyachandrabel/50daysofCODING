4f0a45d513959337bf2ee81dd43f8c176fbbda61
Riya Chandrabel <158443536+riyachandrabel@users.noreply.github.com>
2024-03-06 23:24:23 +0530

    Add files via upload

---
+++ DAY2.py
@@ -0,0 +1,10 @@
+#creating a calculator
+a=50
+b=3
+print("The vlaue of",a,"+",3,"is",a+b)
+print("The vlaue of",a,"-",3,"is",a-b)
+print("The vlaue of",a,"*",3,"is",a*b)
+print("The vlaue of",a,"/",3,"is",a/b)
+print("The vlaue of",a,"%",3,"is",a%b)
+print("The vlaue of",a,"//",3,"is",a//b)
+print("The vlaue of",a,"**",3,"is",a**b)
